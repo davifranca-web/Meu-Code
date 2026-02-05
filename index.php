6a43e3ba3153e65abdc2b93a27da9b92997dce0d
DaviiOliveira <davi.franca@estudante.ifto.edu.br>
2026-02-05 20:37:59 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Minha primeira pagina</title>
+    <link rel="stylesheet" href="assets/styles.css">
+</head>
+
+<body>
+    <div class="container">
+    </div>
+    
+</body>
+</html>
